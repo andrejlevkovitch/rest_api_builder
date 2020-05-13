42d6582699186214a3747b0d7ba692e33fc45846
andrejlevkovitch <andrejlevkovitch@gmail.com>
2020-05-13 11:22:35 +0300

    add checks params in create_endpoint_t function for debug mode

--- src/rest_api_builder.lua
+++ src/rest_api_builder.lua
@@ -380,8 +380,31 @@ end
 --- same as create_endpoint, but take table as argument
 -- @see create_endpoint
 -- @usage api.create_endpoint{api_version = "v1", method = "GET", path_signature = "/hello/<name>", callback = foo}
+-- @warning in debug mode this method can produce error about unexpected key in arg_table - so, if you need append some
+-- new keys for documentation or by other reason, you need override this method. Or don't use debug mode
 function M:create_endpoint_t(arg_table)
   self.assert_arg_type(arg_table, "table", "invalid arg_table")
+
+  if self.is_debug then -- check that all params in table are acceptable
+    local oneOf = function(val, t)
+      for _, item in ipairs(t) do
+        if val == item then
+          return true
+        end
+      end
+      return nil
+    end
+
+    for key in pairs(arg_table) do
+      if not oneOf(key,
+                   {"api_version", "method", "path_signature",
+                    "control_headers", "ignore_body", "body_acceptor",
+                    "callback", "description"}) then
+        self.assert_arg_type(key, "nil",
+                             "DBG - unexpected key in arg_table: " .. key)
+      end
+    end
+  end
 
   return self:create_endpoint(arg_table.api_version, arg_table.method,
                               arg_table.path_signature,
